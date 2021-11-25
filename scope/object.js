2d519e89b9307d16e616dde9b9b756a64670283d
Udhhav Arora <udhhavarora@gmail.com>
2021-11-25 09:38:32 +0000

    Edited oje.js file

--- scope/object.js
+++ scope/object.js
@@ -1,10 +1,10 @@
-var person1 = {
-    name:"Udhhav"
-};
+// var person1 = {
+//     name:"Udhhav"
+// };
 
-var person2 = new Object({name:'UdhhavA', age:23});
-console.log(person1);
-console.log(person2.age);
+// var person2 = new Object({name:'UdhhavA', age:23});
+// console.log(person1);
+// console.log(person2.age);
 
 
 
@@ -12,5 +12,14 @@ function Person(){
     this.name = "Hello, I am inside the function";
     var myname = "udhhav";}
 
-var y = new Person();
-console.log(y);
+// var y = new Person();
+// console.log(y);
+
+var p1 = new Person();
+p1.name = "Kiran";
+var p2 = new Person();
+p2.name = "Ravi";
+
+Person.prototype.geder ="M";
+console.log(p1);
+console.log(p2);
