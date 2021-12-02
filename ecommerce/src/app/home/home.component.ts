291e29614b8fea9b96bd846c9788e0dc629cc1cc
Udhhav Arora <udhhavarora@gmail.com>
2021-12-02 04:26:43 +0000

    directives-1

--- ecommerce/src/app/home/home.component.ts
+++ ecommerce/src/app/home/home.component.ts
@@ -7,8 +7,15 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit {
 
+  isServerLive: boolean=false;
+
+
   constructor(private dataSvc:DataService) {
     console.log(this.dataSvc.countryList);
+   }
+
+   onServerStart(){
+     this.isServerLive=true;
    }
 
   ngOnInit(): void {
